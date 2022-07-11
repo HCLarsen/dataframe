9db9a3217ff1906d5bb051ea5e2cba825c4016bd
Chris Larsen <clarsendev@protonmail.com>
2022-07-11 15:52:07 -0400

    Add basic functionality

--- src/dataframe.cr
+++ src/dataframe.cr
@@ -1,6 +1,59 @@
-# TODO: Write documentation for `Dataframe`
-module Dataframe
+require "csv"
+
+class Dataframe
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  getter headers : Array(String)
+  getter rows : Array(Array(String))
+
+  def initialize(@headers, @rows)
+  end
+
+  def self.from_csv_file(filename : String) : Dataframe
+    file = File.read(filename)
+
+    rows = Array(Array(String)).new
+    CSV.each_row(file) do |row|
+      rows << row
+    end
+
+    headers = rows.shift
+
+    Dataframe.new(headers, rows)
+  end
+
+  def inner_join(other : Dataframe, on : Array(String)) : Dataframe
+    new_headers = (@headers + other.headers).uniq
+    new_rows = Array(Array(String)).new
+
+    indexed = indexed_by(on)
+    indexed_other = other.indexed_by(on)
+
+    indexed.each do |index, row|
+      if match = indexed_other[index]?
+        new_row = Array(String).new(new_headers.size, "")
+        new_hash = Hash.zip(new_headers, new_row)
+
+        new_hash.merge!(Hash.zip(@headers, row))
+        new_hash.merge!(Hash.zip(other.headers, match))
+        new_rows << new_hash.values
+      end
+    end
+
+    Dataframe.new(new_headers, new_rows)
+  end
+
+  def indexed_by(headers) : Hash(String, Array(String))
+    indexes = headers.map { |header| @headers.index(header) }.compact
+
+    hash = Hash(String, Array(String)).new
+
+    @rows.each do |row|
+      values = indexes.map { |i| row[i] }
+      index = values.join
+      hash[index] = row
+    end
+
+    hash
+  end
 end
---
+++ test/dataframe_test.cr
@@ -0,0 +1,36 @@
+require "minitest/autorun"
+
+require "/../src/dataframe"
+
+class DataframeTest < Minitest::Test
+  def test_initializes
+    headers = ["Name", "Age", "Nationality"]
+    rows = [
+      ["Jim", "29", "American"],
+      ["Yuri", "35", "Russian"],
+      ["Murray", "40", "American"]
+    ]
+
+    dataframe = Dataframe.new(headers, rows)
+
+    assert_equal headers, dataframe.headers
+    assert_equal rows, dataframe.rows
+  end
+
+  def test_parses_from_csv
+    dataframe = Dataframe.from_csv_file("./test/files/adults.csv")
+
+    assert_equal ["Name", "Gender", "Nationality"], dataframe.headers
+    assert_equal ["Jim","Male","American"], dataframe.rows[0]
+  end
+
+  def test_joins_dataframes_on_specified_columns
+    kids = Dataframe.from_csv_file("./test/files/kids.csv")
+    school = Dataframe.from_csv_file("./test/files/school.csv")
+
+    joined = kids.inner_join(school, on: ["Name", "Age"])
+
+    assert_equal ["Name", "Age", "Gender", "Grade"], joined.headers
+    assert_equal ["Eddie", "20", "Male", "12"], joined.rows[0]
+  end
+end
