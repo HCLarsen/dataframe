0468f998c321d6760d1ad4e910754026cbe0d2e5
Chris Larsen <clarsendev@protonmail.com>
2022-10-19 17:14:50 -0400

    Organize methods

--- src/dataframe.cr
+++ src/dataframe.cr
@@ -34,16 +34,6 @@ class Dataframe
     Dataframe.new(headers, rows)
   end
 
-  def row_count
-    @rows.size
-  end
-
-  # Outputs the `Dataframe` instance as a string in CSV format.
-  def to_csv
-    output = @headers.map{ |e| %("#{e}") }.join(",") + "\n"
-    return output + @rows.map { |row| row.map{ |e| %("#{e}") }.join(",") + "\n" }.join
-  end
-
   # Returns a `Hash` where the keys are the headers of the `Dataframe` instance,
   # and the values are arrays of the corresponding column.
   def columns : Hash(String, Array(String))
@@ -54,144 +44,6 @@ class Dataframe
     end
 
     columns
-  end
-
-  # Returns a new `Dataframe` that is the result of an inner join of the
-  # receiver and *other*, using the headers in *on* to match rows.
-  def inner_join(other : Dataframe, on : Array(String)) : Dataframe
-    new_headers = (@headers + other.headers).uniq
-    new_rows = Array(Array(String)).new
-
-    indexed = indexed_by(on)
-    indexed_other = other.indexed_by(on)
-
-    indexed.each do |index, row|
-      if match = indexed_other[index]?
-        new_row = Array(String).new(new_headers.size, "")
-        new_hash = Hash.zip(new_headers, new_row)
-
-        new_hash.merge!(Hash.zip(@headers, row))
-        new_hash.merge!(Hash.zip(other.headers, match))
-        new_rows << new_hash.values
-      end
-    end
-
-    Dataframe.new(new_headers, new_rows)
-  end
-
-  # Returns a new `Dataframe` that is the result of a left outer join of the
-  # receiver and *other*, using the headers in *on* to match rows.
-  def left_outer_join(other : Dataframe, on : Array(String)) : Dataframe
-    new_headers = (@headers + other.headers).uniq
-    new_rows = Array(Array(String)).new
-
-    indexed = indexed_by(on)
-    indexed_other = other.indexed_by(on)
-
-    indexed.each do |index, row|
-      new_row = Array(String).new(new_headers.size, "")
-      new_hash = Hash.zip(new_headers, new_row)
-
-      new_hash.merge!(Hash.zip(@headers, row))
-      if match = indexed_other[index]?
-        new_hash.merge!(Hash.zip(other.headers, match))
-      end
-      new_rows << new_hash.values
-    end
-
-    Dataframe.new(new_headers, new_rows)
-  end
-
-  # Returns a new `Dataframe` that is the result of a right outer join of the
-  # receiver and *other*, using the headers in *on* to match rows.
-  def right_outer_join(other : Dataframe, on : Array(String)) : Dataframe
-    other.left_outer_join(self, on: on)
-  end
-
-  # Returns a new `Dataframe` that is the result of a full join of the
-  # receiver and *other*, using the headers in *on* to match rows.
-  def full_join(other : Dataframe, on : Array(String)) : Dataframe
-    new_headers = (@headers + other.headers).uniq
-    new_rows = Array(Array(String)).new
-
-    indexed = indexed_by(on)
-    indexed_other = other.indexed_by(on)
-
-    indexed.each do |index, row|
-      new_row = Array(String).new(new_headers.size, "")
-      new_hash = Hash.zip(new_headers, new_row)
-
-      new_hash.merge!(Hash.zip(@headers, row))
-      if match = indexed_other[index]?
-        new_hash.merge!(Hash.zip(other.headers, match))
-        indexed_other.delete(index)
-      end
-      new_rows << new_hash.values
-    end
-
-    indexed_other.each do |index, row|
-      new_row = Array(String).new(new_headers.size, "")
-      new_hash = Hash.zip(new_headers, new_row)
-      new_hash.merge!(Hash.zip(other.headers, row))
-
-      new_rows << new_hash.values
-    end
-
-    Dataframe.new(new_headers, new_rows)
-  end
-
-  def indexed_by(headers : Array(String)) : Hash(String, Array(String))
-    header_indexes = header_indexes(headers)
-
-    hash = Hash(String, Array(String)).new
-
-    @rows.each do |row|
-      values = header_indexes.map { |i| row[i] }
-      index = values.join
-      hash[index] = row
-    end
-
-    hash
-  end
-
-  # Iterates through all elements in the column specified by *header*, running
-  # the provided block on each element.
-  def modify_column(header : String, & : String ->)
-    new_columns = columns
-
-    new_column = new_columns[header].map do |element|
-      yield element
-    end
-
-    new_columns[header] = new_column
-    @rows = new_columns.values.transpose
-  end
-
-  # Removes all rows for which a previous row is identical in the columns
-  # specified by *headers*.
-  #
-  # If *headers* is left blank, it calculates duplicates based on all columns
-  def remove_duplicates(headers = [] of String)
-    new_rows = Array(Array(String)).new
-
-    if headers.size > 0
-      header_indexes = header_indexes(headers)
-
-      indexes = [] of String
-
-      @rows.each do |row|
-        values = header_indexes.map { |i| row[i] }
-        index = values.join
-        unless indexes.includes?(index)
-          indexes << index
-          new_rows << row
-        end
-      end
-    else
-      new_rows = @rows.uniq
-    end
-
-    @rows = new_rows
   end
 
   # Returns a new dataframe with all duplicate rows from the reciever, based
@@ -246,6 +98,155 @@ class Dataframe
     end
 
     Dataframe.new(self.headers, duplicate_rows)
+  end
+
+  # Returns a new `Dataframe` that is the result of a full join of the
+  # receiver and *other*, using the headers in *on* to match rows.
+  def full_join(other : Dataframe, on : Array(String)) : Dataframe
+    new_headers = (@headers + other.headers).uniq
+    new_rows = Array(Array(String)).new
+
+    indexed = indexed_by(on)
+    indexed_other = other.indexed_by(on)
+
+    indexed.each do |index, row|
+      new_row = Array(String).new(new_headers.size, "")
+      new_hash = Hash.zip(new_headers, new_row)
+
+      new_hash.merge!(Hash.zip(@headers, row))
+      if match = indexed_other[index]?
+        new_hash.merge!(Hash.zip(other.headers, match))
+        indexed_other.delete(index)
+      end
+      new_rows << new_hash.values
+    end
+
+    indexed_other.each do |index, row|
+      new_row = Array(String).new(new_headers.size, "")
+      new_hash = Hash.zip(new_headers, new_row)
+      new_hash.merge!(Hash.zip(other.headers, row))
+
+      new_rows << new_hash.values
+    end
+
+    Dataframe.new(new_headers, new_rows)
+  end
+
+  def indexed_by(headers : Array(String)) : Hash(String, Array(String))
+    header_indexes = header_indexes(headers)
+
+    hash = Hash(String, Array(String)).new
+
+    @rows.each do |row|
+      values = header_indexes.map { |i| row[i] }
+      index = values.join
+      hash[index] = row
+    end
+
+    hash
+  end
+
+  # Returns a new `Dataframe` that is the result of an inner join of the
+  # receiver and *other*, using the headers in *on* to match rows.
+  def inner_join(other : Dataframe, on : Array(String)) : Dataframe
+    new_headers = (@headers + other.headers).uniq
+    new_rows = Array(Array(String)).new
+
+    indexed = indexed_by(on)
+    indexed_other = other.indexed_by(on)
+
+    indexed.each do |index, row|
+      if match = indexed_other[index]?
+        new_row = Array(String).new(new_headers.size, "")
+        new_hash = Hash.zip(new_headers, new_row)
+
+        new_hash.merge!(Hash.zip(@headers, row))
+        new_hash.merge!(Hash.zip(other.headers, match))
+        new_rows << new_hash.values
+      end
+    end
+
+    Dataframe.new(new_headers, new_rows)
+  end
+
+  # Returns a new `Dataframe` that is the result of a left outer join of the
+  # receiver and *other*, using the headers in *on* to match rows.
+  def left_outer_join(other : Dataframe, on : Array(String)) : Dataframe
+    new_headers = (@headers + other.headers).uniq
+    new_rows = Array(Array(String)).new
+
+    indexed = indexed_by(on)
+    indexed_other = other.indexed_by(on)
+
+    indexed.each do |index, row|
+      new_row = Array(String).new(new_headers.size, "")
+      new_hash = Hash.zip(new_headers, new_row)
+
+      new_hash.merge!(Hash.zip(@headers, row))
+      if match = indexed_other[index]?
+        new_hash.merge!(Hash.zip(other.headers, match))
+      end
+      new_rows << new_hash.values
+    end
+
+    Dataframe.new(new_headers, new_rows)
+  end
+
+  # Iterates through all elements in the column specified by *header*, running
+  # the provided block on each element.
+  def modify_column(header : String, & : String ->)
+    new_columns = columns
+
+    new_column = new_columns[header].map do |element|
+      yield element
+    end
+
+    new_columns[header] = new_column
+    @rows = new_columns.values.transpose
+  end
+
+  # Removes all rows for which a previous row is identical in the columns
+  # specified by *headers*.
+  #
+  # If *headers* is left blank, it calculates duplicates based on all columns
+  def remove_duplicates(headers = [] of String)
+    new_rows = Array(Array(String)).new
+
+    if headers.size > 0
+      header_indexes = header_indexes(headers)
+
+      indexes = [] of String
+
+      @rows.each do |row|
+        values = header_indexes.map { |i| row[i] }
+        index = values.join
+        unless indexes.includes?(index)
+          indexes << index
+          new_rows << row
+        end
+      end
+    else
+      new_rows = @rows.uniq
+    end
+
+    @rows = new_rows
+  end
+
+  # Returns a new `Dataframe` that is the result of a right outer join of the
+  # receiver and *other*, using the headers in *on* to match rows.
+  def right_outer_join(other : Dataframe, on : Array(String)) : Dataframe
+    other.left_outer_join(self, on: on)
+  end
+
+  # Returns the number of rows in the `Dataframe`.
+  def row_count
+    @rows.size
+  end
+
+  # Outputs the `Dataframe` instance as a string in CSV format.
+  def to_csv
+    output = @headers.map{ |e| %("#{e}") }.join(",") + "\n"
+    return output + @rows.map { |row| row.map{ |e| %("#{e}") }.join(",") + "\n" }.join
   end
 
   private def header_indexes(headers : Array(String)) : Array(Int32)
